9931187d22546f66feba14d5076f953ab88a8326
rutujadattajadhav <rutujadattajadhav@gmail.com>
2025-01-14 19:06:36 +0530

    splunk configuration

--- src/main/java/org/example/UserLoginApplication.java
+++ src/main/java/org/example/UserLoginApplication.java
@@ -1,14 +1,19 @@
 package org.example;
 
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import reactor.core.publisher.Hooks;
 
+import java.util.UUID;
+
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserLoginApplication {
+public class
+UserLoginApplication {
     public static void main(String[] args) {
+        MDC.put("correlationId", UUID.randomUUID().toString()+"_app_startup_log");
         Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(UserLoginApplication.class,args);
 
---
+++ src/main/java/org/example/filter/CorrelationIdWebFilter.java
@@ -0,0 +1,36 @@
+package org.example.filter;
+
+import org.apache.logging.log4j.ThreadContext;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Component
+public class CorrelationIdWebFilter implements WebFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String correlationId = exchange.getRequest().getHeaders().getFirst("X-Correlation-ID");
+        if (correlationId == null) {
+            correlationId = UUID.randomUUID().toString();
+        }
+
+        // Correctly setting the correlation ID in the ThreadContext for each subscriber
+        String finalCorrelationId = correlationId;
+        MDC.put("correlationId", finalCorrelationId);
+        return chain.filter(exchange)
+                .contextWrite(ctx -> ctx.put("correlationId", finalCorrelationId))  // Adding to Reactor Context
+                .doOnEach(signal -> {
+                    if (!signal.isOnComplete()) {  // Check to avoid accessing context on completion
+                        String cid = signal.getContextView().getOrDefault("correlationId", "default-correlation-id");
+                        ThreadContext.put("correlationId", cid);
+                    }
+                })
+                .doFinally(signalType -> ThreadContext.clearAll());  // Clearing ThreadContext after processing
+    }
+}
